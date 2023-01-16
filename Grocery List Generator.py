6c58231e6c9c541bef166f9b1e09e7f202324004
jnrl12098 <jasonlamzon@gmail.com>
2023-01-17 01:12:38 +0800

    Added editing function and minimized boilerplate

--- Grocery List Generator.py
+++ Grocery List Generator.py
@@ -1,78 +1,135 @@
 #GROCERY LIST GENERATOR
 import os
 
-def display_list(items):                                #function to display list with numbering
+def display_list(items):                                # function to display list with numbering
     counter = 1
     for i in items:
-        print("[" + str(counter) + "] " + i)            #format is "[counter] item"
+        print("[" + str(counter) + "] " + i)            # format is "[counter] item"
         counter += 1
 
-def search_recipe(recipes_list):
-    narrowed_list = []                                      #build a narrowed-down list of recipes based on the search word
-    while len(narrowed_list) == 0:                          #keep searching for recipes if the narrowed-down list is empty (implying that the search word is ineffective)
-        search_word = input("Enter recipe: ").lower()           #set to lower-case for flexibility
-        for i in recipes_list:                                  #for each item in the list of recipes,
-            if i.lower().find(search_word) != -1:                   #if the search word is found in the item,
-                narrowed_list.append(i)                                 #add the item to the narrowed-down list
-        if len(narrowed_list) == 0:                             #if the narrowed-down list is empty after all that,
+def file_as_list(filename, list):                       # function to place contents of a file into a list
+    with open(filename, "r") as file:
+        for i in file.readlines():
+            list.append(i[slice(0,-1)])                 # removes the "\n" at the end
+
+def search_recipe(recipes_list):                        # function to search for a recipe from a list of recipes
+    narrowed_list = []                                      # build a narrowed-down list of recipes based on the search word
+    while len(narrowed_list) == 0:                          # keep searching for recipes if the narrowed-down list is empty (implying that the search word is ineffective)
+        search_word = input("Enter recipe: ").lower()           # set to lower-case for flexibility
+        narrowed_list = [i for i in recipes_list if i.lower().find(search_word) != -1]
+        if len(narrowed_list) == 0:                             # if the resulting narrowed-down list is empty, loop again to try a different search word
             print("Try another word.")
-            continue                                                #search again with a different search word
-        display_list(narrowed_list)                             #display the narrowed-down list of recipes
-        print("[" + str(len(narrowed_list)+1) + "] Try another word/phrase")    #at the end of the list, add an option to search for a different recipe
+            continue
+        display_list(narrowed_list)                             # display the narrowed-down list of recipes
+        print("[" + str(len(narrowed_list)+1) + "] Try another word/phrase")    # at the end of the list, add an option to search for a different recipe
         number = -1                                         #set this variable to -1 for the while-loop
         while number not in range(0, len(narrowed_list)+2):         #while the input number is not found in the range of options,
-            number = int(input("Enter number of choice: "))             #ask the user to enter a number corresponding to an option in the list
-        if number == (len(narrowed_list) + 1):                  #if the user picked the last option,
+            number = int(input("Enter number of choice: ")) - 1          #ask the user to enter a number corresponding to an option in the list
+        if number == len(narrowed_list):                    #if the user picked the last option, ( number = (len(narrowed_list) + 1) - 1 )
             narrowed_list.clear()                                   #by clearing the list, the condition of the while-loop to continue is met as well
         else:                                                   #check if this recipe's text file exists
-            recipe_path = "Recipes\\" + narrowed_list[number - 1] + ".txt"
+            recipe_path = "Recipes\\" + narrowed_list[number] + ".txt"
             if os.path.exists(recipe_path):                         #if the text file of the recipe exists,
-                return narrowed_list[number-1]                          #return the name of the recipe
+                return narrowed_list[number]                            #return the name of the recipe
             else:                                                   #else, choose another recipe
-                print("This recipe doesn't exist!")
+                print("This recipe's file does not exist.")
                 narrowed_list.clear()                                   # by clearing the list, the condition of the while-loop to continue is met as well
+
+def edit_list(dummy_recipe):                          # function to edit a list of items
+    edit_choices = ["Add Items", "Remove Items", "Replace Items", "Done Editing"]
+    while True:
+        print("------------------\nEditing Menu:")
+        display_list(edit_choices)  # display the choices for editing
+        int_choice = 0
+        while int_choice not in range(1, 5):                # use while-loop to make sure user only chooses among choices
+            int_choice = int(input("Enter the number of your choice: "))
+        if int_choice == 1:         # add items
+            while (add_ingredient := input("One at a time, enter the items to add (or type \"Done\" to go back): ")).lower() != "done":
+                dummy_recipe.append(add_ingredient)
+            print("------------------\nUpdated list of items:")      # update the user on the list of ingredients
+            display_list(dummy_recipe)
+        elif int_choice == 2:       #remove items
+            if len(dummy_recipe) == 0:
+                print("There are no more items on the list.")   # if there are no ingredients in the recipe, user shouldn't be able to remove an ingredient
+            else:
+                while (remove_ingredient := input("Enter the item to remove (or type \"Done\" to go back): ")).lower() != "done":
+                    dummy_recipe = [i for i in dummy_recipe if i.lower().find(remove_ingredient) == -1]  # if i.lower().find(remove_ingredient) = -1 then we don't want to remove the ingredient
+                    if len(dummy_recipe) == 0:
+                        print("There are no more items on the list.")
+                        break
+                    else:
+                        print("------------------\nUpdated list of items:")
+                        display_list(dummy_recipe)
+        elif int_choice == 3:       #replace an ingredient with another ingredient
+            if len(dummy_recipe) == 0:
+                print("There are no more items on the list.")   # if there are no ingredients in the recipe, user shouldn't be able to place an ingredient
+            else:
+                while True:
+                    print("------------------\nUpdated list of items:")
+                    display_list(dummy_recipe)
+                    print("[" + str(len(dummy_recipe) + 1) + "] Go back to previous menu.")
+                    edit_index = -1
+                    while edit_index not in range(0, len(dummy_recipe) + 1):
+                        edit_index = int(input("Enter the number of the item to replace (or enter [" + str(len(dummy_recipe) + 1) + "] to go back): ")) - 1
+                    if edit_index == len(dummy_recipe):
+                        break
+                    else:
+                        dummy_recipe[edit_index] = input("Enter the item to replace " + dummy_recipe[edit_index] + ": ")
+        else:
+            break
+    return dummy_recipe
+
 #MAIN
-recipes_text = open("Recipes\\_Recipe List.txt", "r")   #access "_Recipe List.txt" file
-recipes_list = []                                       #build list of recipes using recipes found in text file
-for i in recipes_text.readlines():
-    recipes_list.append(i[slice(0,-1)])                 #slice to remove "\n" from name when generating the list
-recipes_text.close()                                    #close "_Recipe List.txt" file
-
-inventory_text = open("Inventory.txt", "r")             #build list of inventory of ingredients in the same fashion as above
-inventory_list = []
-for i in inventory_text.readlines():
-    inventory_list.append(i[slice(0,-1)])
-inventory_text.close()
-
-yn_choices = ["y", "n"]
-done_adding = False
-grocery_list = []                                       #build the grocery list from scratch, append as needed
-dummy_recipe = []                                       #build the list of ingredients for the recipe
-while done_adding == False:
-    recipe = open("Recipes\\" + search_recipe(recipes_list) + ".txt", "r")
-    for i in recipe.readlines():
-        dummy_recipe.append(i[slice(0,-1)])
-    recipe.close()
-    #insert section where user edits the recipe
-    for i in dummy_recipe:
-        if i not in grocery_list and i not in inventory_list:
+dummy_recipe = []                                       # used to generate an instance of a recipe
+grocery_list = []                                       # build the grocery list from scratch, append one recipe at a time
+recipes_list = []                                       # used to contain list of recipes from text file
+storage_list = []                                       # used to contain list of ingredients in inventory from text file
+# import list of recipes and list of ingredients in inventory
+file_as_list("Recipes\\_Recipe List.txt", recipes_list)
+file_as_list("Inventory.txt", storage_list)
+# add recipes to the grocery list
+while True:
+    dummy_recipe.clear()                                                            # clear the dummy list before starting each iteration
+    file_as_list("Recipes\\" + search_recipe(recipes_list) + ".txt", dummy_recipe)  # generate an instance of the recipe
+    print("These are the ingredients for the recipe:")                              # display the ingredients of the recipe
+    display_list(dummy_recipe)
+    while (choice := input("Edit the recipe? (y/n) ")) not in ["y", "n"]:           # ask the user if they want to edit the recipe
+        pass
+    if choice == "y":                                                               # if yes, enter editing recipe mode; else, continue
+        dummy_recipe = edit_list(dummy_recipe)                                        # update instance of recipe
+        if len(dummy_recipe) == 0:                                                      # in the case where there are no more ingredients, update the user
+            print("All of the ingredients for this recipe have been removed.")
+        else:
+            print("------------------\nUpdated list of ingredients for the recipe:")
+            display_list(dummy_recipe)
+    for i in dummy_recipe:                                                          # add the ingredients of this instance of the recipe to the grocery list
+        if i not in grocery_list and i not in storage_list:
             grocery_list.append(i)
-    choice = None
-    while choice not in yn_choices:
-        choice = input("Add more recipes? (y/n) ")
-    if choice == "n":
-        done_adding = True
-    else:
-        dummy_recipe.clear()
-
-grocery_list.sort()
+    while (choice := input("Add more recipes? (y/n) ")) not in ["y", "n"]:          # ask the user if they want to add more recipes
+        pass
+    if choice == "n":                                                               # if the user doesn't want to add more recipes, proceed to finalizing grocery list
+        break
+# organize and edit the grocery list directly
+grocery_list.sort()                                                             # sort the grocery list before displaying to the user
 print("\nGrocery List:")
-for i in grocery_list:
-    print(i)
-
-#grocery_text = open("Grocery List.txt", "w")
-#grocery_text.close()
-
+display_list(grocery_list)
+while (choice := input("Edit the grocery list? (y/n) ")) not in ["y","n"]:      # ask the user if they want to edit the grocery list
+    pass
+if choice == "y":
+    grocery_list = edit_list(grocery_list)
+    if len(grocery_list) == 0:
+        print("All of the items in the grocery list have been removed.")
+    else:
+        print("------------------\nUpdated grocery list:")
+        display_list(grocery_list)
+if len(grocery_list) > 0:
+    grocery_list.sort()                                                             # sort the grocery list before exporting
+    print("The grocery list has been sorted and exported to \"Grocery List.txt\"")  # export the list to a text file
+    with open("Grocery List.txt", "w") as file:
+        for i in grocery_list:
+            file.write(i + "\n")
+else:
+    print("There's no grocery list to export.")
 
 
 
--- README.txt
+++ README.txt
@@ -1,3 +1,26 @@
+20230116
+Project Version 0.02
+*incorporated walrus operators and list comprehensions to shorten code
+	*note that
+		choice = ""
+		while choice := input() not in ["y","n"] 
+			pass 				
+	results in choice as true or false but
+		choice = ""
+		while (choice := input()) not in ["y","n"]
+			pass
+	results in choice as "y" or "n" 
+*implemented a function to edit (add, remove, replace items) a list e.g. ingredients of a recipe or the items of a grocery list
+	*considers a scenario where the user has emptied the list
+*implemented export of grocery list into a text file
+
+20230113 
+Project Version 0.02 (no commits yet)
+*cleanup of boilerplate code
+	*implemented a function to write the contents of a text file into a list
+	*implemented a function to display the contents of a list with numbering
+*changed while done_adding = False into while True; going to incorporate this style from now on
+
 20230103 8:20PM - 20230104 12:18AM 
 Project Version 0.01
 *implemented search function 
